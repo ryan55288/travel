f2f463b9849056131f160c0134b397d3319e1ef0
ryanke <ryanke@tt186.com>
2022-08-23 23:07:23 +0800

    0823update

--- all.js
+++ all.js
@@ -1,28 +1,41 @@
-var Lingyalist = document.querySelector(".LingyaBtn");
-Lingyalist.addEventListener("click", LingyaChangelist, false);
+//綁定DOM元素
+var seletBtn = document.querySelector(".seletBtn");
+var middleName = document.querySelector(".middleName");
 
-function LingyaChangelist() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("post", "https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json");
-    xhr.setRequestHeader("content-type", "application/json");
-    var data = JSON.stringify(xhr);
-    xhr.send(data);
-    xhr.onload = function () {
-        var callbackData = JSON.parse(xhr.responseText);
-        console.log(callbackData);
+var seletvalue = document.getElementById("seletvalue");
 
-        // if (callbackData == "苓雅區") {
-        // }
-    };
+
+//監聽改變時的動作
+seletvalue.addEventListener("change", seletchange, false);
+
+
+//打API取值整理資料
+var xhr = new XMLHttpRequest();
+xhr.open("get", "https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json");
+xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+xhr.send(null);
+xhr.onload = function () {
+    var data = JSON.parse(xhr.response);
+    newdata = data.result.records;
+    var ZoneArr = [];
+    ZoneArr = newdata.map(function (item) {
+        return item.Zone;
+    });
+    var onlyZoneArr = ZoneArr.filter(function (item, index, array) {
+        return array.indexOf(item) == index;
+    });
+    onlyZoneArr.unshift("--請選擇行政區--");
+    // console.log(onlyZoneArr)
+    var str = "";
+    for (let i = 0; i < onlyZoneArr.length; i++) {
+        str += `<option value="${onlyZoneArr[i]}" >${onlyZoneArr[i]}</option>`;
+        seletchange()
+    }
+    seletBtn.innerHTML = str;
+};
+
+//點選時做判斷分類
+function seletchange(e) {
+    var selet = e.target.value;
+     
 }
-
-// function changelist(){
-//     var select = e.target.value;
-//     var str = "";
-//     for (var i = 0; i < counlen; i++) {
-//         if (select == country[i].place) {
-//             str += "<li>" + country[i].farmer + "</li>";
-//         }
-//     }
-//     list.innerHTML = str;
-// }
